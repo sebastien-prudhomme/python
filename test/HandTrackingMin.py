c69073c39d852cea69e095524b50fb807fb14cba
Sébastien Prud'homme <sebastien.prudhomme@gmail.com>
2021-09-09 16:54:17 +0200

    WIP

--- test/HandTrackingMin.py
+++ test/HandTrackingMin.py
@@ -4,8 +4,20 @@ import time
 
 capture = cv2.VideoCapture(0)
 
+mediapipeHands = mediapipe.solutions.hands
+hands = mediapipeHands.Hands()
+
+mediapipeDraw = mediapipe.solutions.drawing_utils
+
 while True:
     success, img = capture.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLandmarks in results.multi_hand_landmarks:
+            mediapipeDraw.draw_landmarks(img, handLandmarks, mediapipeHands.HAND_CONNECTIONS)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
